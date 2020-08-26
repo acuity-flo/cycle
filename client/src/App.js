e452943f47581b023592d4778b5a704b5633f186
Lauren <laurenpitruz@gmail.com>
2020-08-26 15:37:24 -0400

    feat: navbar navigation and logout synced to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { useDispatch, connect } from 'react-redux'
 import { Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
 //Thunks
-import { authMe, logout } from './store'
+import { authMe } from './store'
 
 //Component Imports
 import CalendarView from './components/Calendar'
@@ -29,12 +29,12 @@ function App(props) {
   return (
     <Router>
       <NavBar />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path = "/me" component={UserProfile}/>
-          <Route exact path = "/calendar" component={CalendarView}/>
-          <Route exact path = "/charts" component={ChartHome}/>
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path = "/me" component={UserProfile}/>
+        <Route exact path = "/calendar" component={CalendarView}/>
+        <Route exact path = "/charts" component={ChartHome}/>
+      </Switch>
     </Router>
     // <Fragment>
     //   <button onClick={() => dispatch(logout())}>Logout</button>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,23 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, connect } from 'react-redux'
+import { withRouter } from 'react-router'
+//material UI imports
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
-
-// export default function Navbar () {
-//     return (
-//         <h1>NAV</h1>
-//     )
-// }
+//thunk
+import { logout } from '../store'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,67 +25,82 @@ const useStyles = makeStyles((theme) => ({
       flexGrow: 1,
     },
   }));
-  
-  export default function Navbar() {
-    const classes = useStyles();
-    const [auth, setAuth] = React.useState(true);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-  
-    const handleChange = (event) => {
-      setAuth(event.target.checked);
-    };
-  
-    const handleMenu = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-  
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            {auth && (
-              <div>
-                <IconButton
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={open}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={handleClose}>My Charts</MenuItem>
-                  <MenuItem onClick={handleClose}>Calendar</MenuItem>
-                  <MenuItem onClick={handleClose}>Logout</MenuItem>
-                </Menu>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
+
+function Navbar(props) {
+  const dispatch = useDispatch()
+  const classes = useStyles();
+  const auth = props.isLoggedIn
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleRedirect = (name) => {
+    props.history.push(`/${name}`)
+    handleClose()
   }
+
+  const handleLogout = () => {
+    dispatch(logout())
+    props.history.push('/')
+    handleClose()
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={() => handleRedirect('me')}>Profile</MenuItem>
+                <MenuItem onClick={() => handleRedirect('charts')}>My Charts</MenuItem>
+                <MenuItem onClick={() => handleRedirect('calendar')}>Calendar</MenuItem>
+                <MenuItem onClick={() => handleLogout()}>Logout</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+const mapState = state => ({
+  authUser: state,
+  isLoggedIn: !!state._id
+})
+
+export default withRouter(connect(mapState)(Navbar))
