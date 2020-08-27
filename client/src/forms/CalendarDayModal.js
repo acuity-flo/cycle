8312333b534a32ea5339633721210a5ebd4e9cf4
Olivia Kasmin <oliviakasmin@gmail.com>
2020-08-27 15:45:52 -0400

    feat: calendar modal start

--- client/src/forms/CalendarDayModal.js
+++ client/src/forms/CalendarDayModal.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import moment from 'moment';
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  InputLabel,
-  Select,
-  MenuItem,
-  Modal,
-} from '@material-ui/core';
+// import React, { useState, useEffect } from 'react';
+// import moment from 'moment';
+// import {
+//   Button,
+//   FormControl,
+//   FormLabel,
+//   Input,
+//   InputLabel,
+//   Select,
+//   MenuItem,
+//   Modal,
+// } from '@material-ui/core';
 
-const DayModal = (props) => {
-  return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="simple-modal-title"
-      aria-describedby="simple-modal-description"
-    >
-      <button type="button" onClick={() => console.log('in modal button')}>
-        Add Period
-      </button>
-      <button type="button" onClick={() => console.log('in modal button')}>
-        Add Symptom
-      </button>
-      <button type="button" onClick={() => console.log('in modal button')}>
-        Add Finance
-      </button>
-    </Modal>
-  );
-};
+// // props = {date, open, handleClose}
+
+// const DayModal = (props) => {
+//   return (
+//     <Modal
+//       open={props.open}
+//       onClose={props.handleClose}
+//       aria-labelledby="simple-modal-title"
+//       aria-describedby="simple-modal-description"
+//     >
+//       {/* <button type="button" onClick={() => console.log('in modal button')}>
+//         Add Period
+//       </button>
+//       <button type="button" onClick={() => console.log('in modal button')}>
+//         Add Symptom
+//       </button>
+//       <button type="button" onClick={() => console.log('in modal button')}>
+//         Add Finance
+//       </button> */}
+//     </Modal>
+//   );
+// };
+
+// export default DayModal;
