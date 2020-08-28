d8e4830afcd227a5794e182b7fc1b00b1733ab14
Olivia Kasmin <oliviakasmin@gmail.com>
2020-08-28 11:32:11 -0400

    user model: updated and unused code file

---
+++ client/src/dataVis/SymptomChart.js
@@ -0,0 +1,4 @@
+// const mood = ['stressed', 'motivated', 'calm', 'unmotivated']
+// const emotion = ['happy', 'sad', 'angry', 'frustrated', 'anxious']
+// const pain = ['cramp', 'headache', 'back']
+// const physical = ['nausea','bloating', 'indigestion','fatigue','snacky','PMS']
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -60,67 +60,14 @@ const userSchema = new Schema({
       symptom: [
         {
           type: String,
-          tag: {
-            enum: ['mood', 'emotion', 'pain', 'other', 'custom'],
+          category: {
+            enum: ['mood', 'emotion', 'pain', 'physical', 'custom'],
             default: 'custom',
           },
         },
       ],
     },
   ],
-  symptom: {
-    mood: [
-      {
-        date: { type: Date, default: Date.now },
-        typeOfMood: [
-          {
-            type: String,
-            enum: ['stressed', 'motivated', 'calm', 'unmotivated'],
-          },
-        ],
-      }, // selectable list of strings to store into an array to be able to choose multiple
-    ], //dropdown?
-    emotion: [
-      {
-        date: { type: Date, default: Date.now },
-        typeOfEmotion: [
-          {
-            type: String,
-            enum: ['happy', 'sad', 'angry', 'frustrated', 'anxious'],
-          },
-        ],
-      },
-    ], // selectable list of strings to store into an array to be able to choose multiple
-    pain: [
-      {
-        date: { type: Date, default: Date.now },
-        typeOfPain: [
-          {
-            type: String,
-            enum: ['cramp', 'headache', 'back'],
-          },
-        ],
-      },
-    ], // selectable list of strings to store into an array to be able to choose multiple
-    other: [
-      {
-        date: { type: Date, default: Date.now },
-        typeOther: [
-          {
-            type: String,
-            enum: [
-              'nausea',
-              'bloating',
-              'indigestion',
-              'fatigue',
-              'snacky',
-              'PMS',
-            ],
-          },
-        ],
-      },
-    ],
-  },
 });
 
 userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
@@ -128,10 +75,3 @@ userSchema.plugin(passportLocalMongoose, { usernameField: 'username' });
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-// demographic: {
-//   idenfity: {type: String},
-//   race: {type: String, enum: []},
-//   income: {type: String, enum: ['0-15','15-30k', '30-45', '45-60', '' ]},
-//   city: {},
-// },
