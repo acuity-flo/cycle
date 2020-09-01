be75711c871e82bfd121271eaa1781d1774eaeb3
Olivia Kasmin <oliviakasmin@gmail.com>
2020-09-01 09:12:12 -0400

    util: added finance and period functions

--- utilFcn.js
+++ utilFcn.js
@@ -1,4 +1,4 @@
-// const symptomArr = [
+// const symptomData = [
 //   {
 //     date: '06-06-20',
 //     symptoms: [
@@ -17,8 +17,8 @@
 //   },
 // ];
 
-const UTIL_SYMPTOM = (symptomArr, start, end) => {
-  const symptomObj = symptomArr.reduce(
+const UTIL_SYMPTOM = (symptomData, start, end) => {
+  const symptomObj = symptomData.reduce(
     (acc, el) => {
       if (moment(el.date).isBetween(start, end)) {
         el.symptoms.filter((innerEl) => {
@@ -61,5 +61,66 @@ const UTIL_SYMPTOM = (symptomArr, start, end) => {
   );
   return symptomObj;
 };
+// assign arbitrary values when charting by mapping over?
 
-// console.log(UTIL_SYMPTOM(symptomArr));
+const UTIL_FINANCE = (financeData, start, end) => {
+  const financeObj = financeData.reduce(
+    (acc, el) => {
+      if (moment(el.date).isBetween(start, end)) {
+        let vals = el.purchases.reduce(
+          (innerAcc, innerEl) => {
+            if (innerEl.typeOfPurchase === 'prescription')
+              innerAcc.prescription += innerEl.cost;
+            if (innerEl.typeOfPurchase === 'sanitary product')
+              innerAcc.sanitaryProduct += innerEl.cost;
+            if (innerEl.typeOfPurchase === 'doctor')
+              innerAcc.doctor += innerEl.cost;
+            if (innerEl.typeOfPurchase === 'other')
+              innerAcc.other += innerEl.cost;
+            return innerAcc;
+          },
+          { prescription: 0, sanitaryProduct: 0, doctor: 0, other: 0 }
+        );
+        acc.prescription.push(vals.prescription);
+        acc.sanitaryProduct.push(vals.sanitaryProduct);
+        acc.doctor.push(vals.doctor);
+        acc.other.push(vals.other);
+        acc.x.push(moment(el.date).format('MM-DD-YYYY'));
+      }
+      return acc;
+    },
+    { prescription: [], sanitaryProduct: [], doctor: [], other: [], x: [] }
+  );
+  return financeObj;
+};
+
+const UTIL_PERIOD = (periodData, start, end) => {
+  const flow = (element) => {
+    if (element.typeOfFlow === 'spotting') {
+      element.typeOfFlow = '1';
+    } else if (element.typeOfFlow === 'light') {
+      element.typeOfFlow = '2';
+    } else if (element.typeOfFlow === 'medium') {
+      element.typeOfFlow = '3';
+    } else if (element.typeOfFlow === 'heavy') {
+      element.typeOfFlow = '4';
+    }
+    element.date = moment(element.date).format('MM-DD-YYYY');
+    return element;
+  };
+
+  const flowObj = periodData.reduce(
+    (acc, el) => {
+      if (moment(el.date).isBetween(start, end)) {
+        let data = flow(el);
+        acc.flow.push(data.typeOfFlow);
+        acc.x.push(data.date);
+      }
+      return acc;
+    },
+    { flow: [], x: [] }
+  );
+  return flowObj;
+};
+
+module.exports = { UTIL_FINANCE, UTIL_PERIOD, UTIL_SYMPTOM };
