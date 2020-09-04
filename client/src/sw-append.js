a8c6ad3be9c5ae46cd87218f8de2748aff8143a1
Lauren <laurenpitruz@gmail.com>
2020-09-04 16:59:23 -0400

    still working on deploy

--- client/src/sw-append.js
+++ client/src/sw-append.js
@@ -10,54 +10,54 @@ self.addEventListener('install', evt => {
   console.log('this is a install request')
 })
 
-self.addEventListener('activate', () => {
-  console.log('this is a activate request')
-  let request = indexedDB.open('CYCLE_EXAMPLE', 1), db, tx, store;
+// self.addEventListener('activate', () => {
+//   console.log('this is a activate request')
+//   let request = indexedDB.open('CYCLE_EXAMPLE', 1), db, tx, store;
 
-  request.onupgradeneeded = function(event) {
-    db = request.result
-    store = db.createObjectStore("CYCLE_STORE", {keyPath: "_id"})
-  }
-  request.onsuccess = function(event) {
-      console.log('[onsuccess]', request.result);
-      db = request.result
-      tx = db.transaction('CYCLE_STORE', 'readwrite')
-      store = tx.objectStore('CYCLE_STORE')
+//   request.onupgradeneeded = function(event) {
+//     db = request.result
+//     store = db.createObjectStore("CYCLE_STORE", {keyPath: "_id"})
+//   }
+//   request.onsuccess = function(event) {
+//       console.log('[onsuccess]', request.result);
+//       db = request.result
+//       tx = db.transaction('CYCLE_STORE', 'readwrite')
+//       store = tx.objectStore('CYCLE_STORE')
 
-      db.onerror = function(event) {
-        console.log('CYCLE_STORE ERROR', event.target.errorCode)
-      }
-      store.put({
-        _id: 12345,
-        username: "cherisecycles",
-        name: "cherise",
-        email: "cherise@dummy.com",
-        password: "password",
-        pronouns: "she/her/hers",
-        period: [
-          { date: "2020-06-05", "typeOfFlow": "heavy" },
-          { date: "2020-06-06", "typeOfFlow": "light" },
-        ],
-        symptomTags: [
-          {
-            date: "2020-06-05",
-            symptoms: [
-              { "symptomName": "calm", "category": "mood" },
-              { "symptomName": "stressed", "category": "mood" },
-              { "symptomName": "anxious", "category": "emotion" }
-            ]
-          }
-        ]
-        })
+//       db.onerror = function(event) {
+//         console.log('CYCLE_STORE ERROR', event.target.errorCode)
+//       }
+//       store.put({
+//         _id: 12345,
+//         username: "cherisecycles",
+//         name: "cherise",
+//         email: "cherise@dummy.com",
+//         password: "password",
+//         pronouns: "she/her/hers",
+//         period: [
+//           { date: "2020-06-05", "typeOfFlow": "heavy" },
+//           { date: "2020-06-06", "typeOfFlow": "light" },
+//         ],
+//         symptomTags: [
+//           {
+//             date: "2020-06-05",
+//             symptoms: [
+//               { "symptomName": "calm", "category": "mood" },
+//               { "symptomName": "stressed", "category": "mood" },
+//               { "symptomName": "anxious", "category": "emotion" }
+//             ]
+//           }
+//         ]
+//         })
 
-      tx.oncomplete = function() {
-        db.close()
-      }
-  };
-  request.onerror = function(event) {
-      console.log('[onerror]', request.error);
-  };
-})
+//       tx.oncomplete = function() {
+//         db.close()
+//       }
+//   };
+//   request.onerror = function(event) {
+//       console.log('[onerror]', request.error);
+//   };
+// })
 
 self.addEventListener('fetch', evt => {
   console.log('this is a fetch request')
